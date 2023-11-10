92f304b71dd00cf48f57b933bf8f7ddf1d3adb43
Iryu-0817 <iryu20030817@gmail.com>
2023-11-10 19:23:29 +0900

    Add a file

---
+++ starter-kit/app.js
@@ -0,0 +1,9 @@
+const logAll = (array) => {
+    for( let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+logAll([1,2,3,4,5]);
+logAll([1,2,3,4,5,6]);
+logAll([1,2,3,4,5,6,7]);
