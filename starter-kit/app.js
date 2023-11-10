7bdabe232d063a960f822a49f54174bd4b0d0ead
Iryu-0817 <iryu20030817@gmail.com>
2023-11-11 02:05:53 +0900

    O(log n)記法のコードを記入

--- starter-kit/app.js
+++ starter-kit/app.js
@@ -1,12 +1,24 @@
-const addAndLog = (array) => {
+const binarySearch = (array, key) => {
+    var low = 0;
+    var high = array.length - 1;
+    var mid;
+    var element;
 
-    for( let i = 0; i < array.length; i++) {
-        for( let j = 0; j < array.length; j++) {
-            console.log(array[i] + array[j]);
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2, 10);
+        element = array[mid];
+
+        if(element < key) {
+            low = mid + 1;
+        } else if (element > key) {
+            high = mid - 1; 
+        } else {
+            return mid;
         }
     }
-} 
 
-addAndLog(["A","B","C"]);
-addAndLog(["A","B","C", "D"]);
-addAndLog(["A","B","C", "D", "E"]);
+return -1;
+
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 3));
