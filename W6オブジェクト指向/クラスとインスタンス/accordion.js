d5ca495ff054e5802fd88a24db1d8c1807cd004e
Iryu-0817 <iryu20030817@gmail.com>
2023-11-23 15:32:08 +0900

    クラスまで書いた。次はインスタンスを書く

---
+++ W6オブジェクト指向/クラスとインスタンス/accordion.js
@@ -0,0 +1,44 @@
+(() => {
+
+    class Accordion {
+        //constructor=初期化
+        constructor() {
+            const $elm = document.querySelector('#js-accordion');
+    const $trigger = $elm.getElementsByTagName('a')
+
+
+    const $triggerLen = $trigger.length
+    let index = 0;
+    while (index < $triggerLen) {
+        $trigger[index].addEventListener('click', (e) => clickHandler(e)); 
+        index++;
+            }
+        }
+
+         clickHandler = (e) => {
+        
+            e.preventDefault();
+    
+            const $target = e.currentTarget;
+    
+            const $content = $target.nextElementSibling;
+            if($content.style.display === 'block'){
+                $content.style.display = 'none';
+            } else {
+                $content.style.display = 'block';
+            }
+        };
+    }
+
+    
+
+
+
+
+    
+        
+    
+
+})();
+
+
