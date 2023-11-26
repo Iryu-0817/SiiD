ed6658eef36edb069283737dc961dec5a82500fc
Iryu-0817 <iryu20030817@gmail.com>
2023-11-26 16:53:17 +0900

    Q4Personという名前のクラスを作成してください。このクラスには以下の条件を満たすconstru ctorメソッドとsayHelloメソッドを実装してください。という問題を解いた。

--- W6オブジェクト指向/授業/app.js
+++ W6オブジェクト指向/授業/app.js
@@ -14,4 +14,10 @@ class Person {
 
 let john = new Person("john");
 
+
+/* ここでjohn.sayHelloが使われているのは、他のオブジェクトと区別するためであって、例えば、
+new Person("Alice").sayHello() と new Person("Bob").sayHello() は違う挨拶文を生成
+する。そのため、オブジェクトごとに異なる状態やデータを保持している場合、それらにアクセスするため
+にはメソッドをオブジェクトと結びつけて呼び出す必要があるため、「john.〜」となる。
+*/
 console.log(john.sayHello());  // Outputs: "John says hello!"
