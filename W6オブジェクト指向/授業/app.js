c8c27200b15cec8097ac7d2f95525692c6349e52
Iryu-0817 <iryu20030817@gmail.com>
2023-11-26 17:46:35 +0900

    Q5:Personという名前のクラスを作成してください。このクラスは以下の条件を満たす constructorメソッドとspeakメソッドを実装してください。の問題を解いた。

--- W6オブジェクト指向/授業/app.js
+++ W6オブジェクト指向/授業/app.js
@@ -1,23 +1,19 @@
-
-class Person {
-    constructor(name) {
+ 
+ class Person {
+    constructor (name, age) {
         this.name = name;
+        this.age = age;
+        // 復習だが、「this.name」と指定することで他クラスに要素を引き継ぐことができる。
     }
 
-    sayHello() {
-        // 「` `」と「' '」を間違えないようにする
-        return `${this.name} says hello!`;
+    speak() {
+        // これは条件演算子。詳しくはNotionでまとめたものをみよう
+        let ifmyAge = (this.age < 20) ? '僕':'私'
+        return `${ifmyAge}は${this.name}です。${this.age}です。よろしくお願いします！！`;
     }
-}      
+    
+ }
 
 
-
-let john = new Person("john");
-
-
-/* ここでjohn.sayHelloが使われているのは、他のオブジェクトと区別するためであって、例えば、
-new Person("Alice").sayHello() と new Person("Bob").sayHello() は違う挨拶文を生成
-する。そのため、オブジェクトごとに異なる状態やデータを保持している場合、それらにアクセスするため
-にはメソッドをオブジェクトと結びつけて呼び出す必要があるため、「john.〜」となる。
-*/
-console.log(john.sayHello());  // Outputs: "John says hello!"
+let myAge = new Person ('Ryuya', 20);
+console.log(myAge.speak());
