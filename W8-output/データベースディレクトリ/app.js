6c501ac254e0bb13aa1f5b3c6d966c15c29ce16a
Iryu-0817 <iryu20030817@gmail.com>
2023-12-06 16:08:02 +0900

    説明を追加

--- W8-output/データベースディレクトリ/app.js
+++ W8-output/データベースディレクトリ/app.js
@@ -3,6 +3,8 @@ window.onload = () => {
     let savedEmail = localStorage.getItem('userEmail');
     let savedPassword = localStorage.getItem('password');
     
+
+    // もし二つの内容を保持していたらという内容
     if (savedEmail && savedPassword) {
         document.getElementById('email').value = savedEmail;
         document.getElementById('password').value = savedPassword;
@@ -20,4 +22,7 @@ myButton.addEventListener('click', () => {
     localStorage.setItem('userEmail', userEmail);
     localStorage.setItem('password', password);
 
+    // 入力した内容を保存し、次回入力されるようにするコード
 })
+
+// これらのコードはセキュリティー上懸念があるため、本物の認証システムはサーバー側でやるべき。
