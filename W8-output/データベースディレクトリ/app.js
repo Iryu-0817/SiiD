ac5b1dbb82d2fc4a48b314184785dfff71c3e59b
Iryu-0817 <iryu20030817@gmail.com>
2023-12-06 14:39:56 +0900

    ログインフォームを拡張し、emailとpasswordを画面ロード時に入力されるように した。

--- W8-output/データベースディレクトリ/app.js
+++ W8-output/データベースディレクトリ/app.js
@@ -1,1 +1,23 @@
-console.log("Hello World!!")
+
+window.onload = () => {
+    let savedEmail = localStorage.getItem('userEmail');
+    let savedPassword = localStorage.getItem('password');
+    
+    if (savedEmail && savedPassword) {
+        document.getElementById('email').value = savedEmail;
+        document.getElementById('password').value = savedPassword;
+    }    
+}
+
+
+
+let myButton = document.getElementById('button');
+myButton.addEventListener('click', () => {
+    let userEmail = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+
+    localStorage.setItem('userEmail', userEmail);
+    localStorage.setItem('password', password);
+
+})
